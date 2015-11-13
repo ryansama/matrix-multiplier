338e2699fb63b16be145e07504769ad30f77c499
Ryan <ryan.samarajeewa@gmail.com>
2015-11-13 00:52:23 -0800

    Add calculateProduct() and genMatrixInputField()

--- script.js
+++ script.js
@@ -3,16 +3,44 @@ console.log("Welcome to the matrix multiplier.");
 $(function() {
 
   $( "#submitDim" ).click(function() {
-    console.log("clicked");
-    console.log(getDimentions());
+    //console.log("clicked");
+    var dimentions = getDimentions();
+    $( ".selectDimentions" ).empty();
+    genMatrixInputFields(dimentions);
   });
 
 });
 
-gen
+function genMatrixInputFields(dimentions){
+  $("body").append("<div class=\"inputField\"></div>");
+  $(".inputField").append("<h3>Matrix A</h3>");
+  $(".inputField").append("<br>");
+  console.log(dimentions[0][0]);
+  console.log(dimentions[0][1]);
+  for(var i = 0; i < dimentions[0][0]; i++){
+    for(var j = 0; j < dimentions[0][1]; j++){
+      $(".inputField").append("<input type=\"text\" name=\"mytext\"/>");
+      //console.log(i + " " + j);
+    }
+    $(".inputField").append("<br>");
+  }
+
+  $(".inputField").append("<h3>Matrix B</h3>");
+  $(".inputField").append("<br>");
+
+  for(var i = 0; i < dimentions[1][0]; i++){
+    for(var j = 0; j < dimentions[1][1]; j++){
+      $(".inputField").append("<input type=\"text\" name=\"mytext\"/>");
+    }
+    $(".inputField").append("<br>");
+  }
+
+  $(".inputField").append("<button id=\"calcualte\">Calculate</button>")
+
+}
 
 function getDimentions(){
-  var dimentions = [[0,0],[0,0]];
+  var dimentions = [[1,1],[1,1]];
   dimentions[0][0] = Number($( "#Arows" ).val());
   dimentions[0][1] = Number($( "#Acolumns" ).val());
   dimentions[1][0] = Number($( "#Brows" ).val());
@@ -20,20 +48,18 @@ function getDimentions(){
   return dimentions;
 }
 
-function calculateProduct(){
-  for(int i = 0; i < matrixK.length; i++){
+function calculateProduct(matrixK, matrixJ){
+  var matrix = new Array();
+  var dotProduct = 0;
 
-    for(int k = 0; k < matrixJ[0].length;k++){
-
-     for(int j = 0; j < matrix[0+l][0].length; j++){
-
-      dotProduct += matrixK[i][j] * matrixJ[j][k];
-
-     }
-
-     matrix[i][k] = dotProduct;
-     dotProduct = 0;
-
+  for(var i = 0; i < matrixK.length; i++){
+    for(var k = 0; k < matrixJ[0].length;k++){
+      for(var j = 0; j < matrixK[0].length; j++){
+        dotProduct += matrixK[i][j] * matrixJ[j][k];
+      }
+      matrix[i][k] = dotProduct;
+      dotProduct = 0;
     }
    }
+
 }
